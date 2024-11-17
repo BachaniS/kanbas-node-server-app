b88b7f94cd4b18b8195b62bed17ef30be3716be5
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-11-17 01:17:13 -0500

    add DELETE and PUT routes for managing todos in Lab5

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -40,6 +40,12 @@ export default function WorkingWithArrays(app) {
     todos.splice(todoIndex, 1);
     res.json(todos);
   });
+  app.delete("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    todos.splice(todoIndex, 1);
+    res.sendStatus(200);
+  });
   app.get("/lab5/todos/:id/title/:title", (req, res) => {
     const { id, title } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
@@ -61,6 +67,16 @@ export default function WorkingWithArrays(app) {
       todo.description = description;
     }
     res.json(todos);
+  });
+  app.put("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    todos = todos.map((t) => {
+      if (t.id === parseInt(id)) {
+        return { ...t, ...req.body };
+      }
+      return t;
+    });
+    res.sendStatus(200);
   });
 
 }
