63d5958939b08b2c0af7d17314ba089c105682d4
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-11-18 09:29:24 -0500

    add express-session for user session management and implement user data access methods

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-import express from 'express';
+import express from "express";
+import session from "express-session";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
 const app = express();
-app.use(cors());
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(session(sessionOptions));
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+);
 app.use(express.json());
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
