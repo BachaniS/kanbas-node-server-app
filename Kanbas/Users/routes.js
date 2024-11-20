966d5e344105f678d2ff576022bd3f66db143b41
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-11-19 23:05:08 -0500

    add user course management routes and session handling

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -1,6 +1,21 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+import * as courseDao from "../Courses/dao.js";
+
 export default function UserRoutes(app) {
+  const findCoursesForEnrolledUser = (req, res) => {
+    let { userId } = req.params;
+    if (userId === "current") {
+      const currentUser = req.session["currentUser"];
+      if (!currentUser) {
+        res.sendStatus(401);
+        return;
+      }
+      userId = currentUser._id;
+    }
+    const courses = courseDao.findCoursesForEnrolledUser(userId);
+    res.json(courses);
+  };
+  app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
   const createUser = (req, res) => { };
   const deleteUser = (req, res) => { };
   const findAllUsers = (req, res) => { };
@@ -9,7 +24,8 @@ export default function UserRoutes(app) {
     const userId = req.params.userId;
     const userUpdates = req.body;
     dao.updateUser(userId, userUpdates);
-    currentUser = dao.findUserById(userId);
+    const currentUser = dao.findUserById(userId);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
    };
   const signup = (req, res) => {
@@ -19,20 +35,30 @@ export default function UserRoutes(app) {
         { message: "Username already in use" });
       return;
     }
-    currentUser = dao.createUser(req.body);
-    res.json(currentUser);
+    const currentUser = dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
    };
-  const signin = (req, res) => {
+   const signin = (req, res) => {
     const { username, password } = req.body;
-    currentUser = dao.findUserByCredentials(username, password);
-    res.json(currentUser);
-   };
+    const currentUser = dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json({ message: "Unable to login. Try again later." });
+    }
+  };
    const signout = (req, res) => {
-    currentUser = null;
+    req.session.destroy();
     res.sendStatus(200);
   };
 
   const profile = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
     res.json(currentUser);
   };
 
