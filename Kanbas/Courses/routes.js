6a36d33a05fbf8b83e233dd006939e39ffc041e0
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-12-01 05:01:11 -0500

    refactor course and enrollment management to use mongoose models for CRUD operations and update routes for async handling

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,11 +1,12 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
+import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function CourseRoutes(app) {
-  app.get("/api/courses/:courseId/modules", (req, res) => {
+  app.get("/api/courses/:courseId/modules", async(req, res) => {
     const { courseId } = req.params;
-    const modules = modulesDao.findModulesForCourse(courseId);
+    const modules = await modulesDao.findModulesForCourse(courseId);
     res.json(modules);
   });
   app.get("/api/courses/:courseId/assignments", (req, res) => {
@@ -28,13 +29,13 @@ export default function CourseRoutes(app) {
     const status = await dao.updateCourse(courseId, courseUpdates);
     res.send(status);
   });
-  app.post("/api/courses/:courseId/modules", (req, res) => {
+  app.post("/api/courses/:courseId/modules", async(req, res) => {
     const { courseId } = req.params;
     const module = {
       ...req.body,
       course: courseId,
     };
-    const newModule = modulesDao.createModule(module);
+    const newModule =await modulesDao.createModule(module);
     res.send(newModule);
   });
   app.post("/api/courses/:courseId/assignments", (req, res) => {
@@ -48,6 +49,10 @@ export default function CourseRoutes(app) {
   });
   app.post("/api/courses", async (req, res) => {
     const course = await dao.createCourse(req.body);
+    const currentUser = req.session["currentUser"];
+    if(currentUser){
+     await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+    }
     res.json(course);
   });
  
--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,23 +1,21 @@
-import Database from "../Database/index.js";
+import model from "./model.js";
 
 export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  enrollments.push({ _id: Date.now(), user: userId, course: courseId });
+  return model.create({ user: userId, course: courseId });
 }
 export function unenrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  Database.enrollments = enrollments.filter(enrollment => enrollment.user !== userId || enrollment.course !== courseId);
+  return model.deleteOne({ user: userId, course: courseId });
 }
-export function findCoursesForEnrolledUser(userId) {
-  const { enrollments } = Database;
-  return enrollments.filter(enrollment => enrollment.user === userId);
+export async function findCoursesForEnrolledUser(userId) {
+  const enrollments = await model.find({ user: userId }).populate("course");
+  return enrollments.map((enrollment) => enrollment.course);
 }
 
 export function findAllEnrollments() {
-  return Database.enrollments;
+  return model.find();
 }
 
-export function findUsersForEnrolledCourse(courseId) {
-  const { enrollments } = Database;
-  return enrollments.filter(enrollment => enrollment.course === courseId);
+export async function findUsersForEnrolledCourse(courseId) {
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments.map((enrollment) => enrollment.user);
 }
