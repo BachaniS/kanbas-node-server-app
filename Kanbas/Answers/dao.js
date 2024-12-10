069427b25746b2f294d1738babb3481e00055f34
BachaniS <bachani.s@northeastern.edu>
2024-12-10 02:35:07 -0500

    refactor findLatestAnswersForQuiz function; remove console logs and comments for cleaner code

--- Kanbas/Answers/dao.js
+++ Kanbas/Answers/dao.js
@@ -10,24 +10,19 @@ export function findAnswersForQuiz(quizId, userId, attempt) {
 }
 
 export async function findLatestAnswersForQuiz(quizId, userId) {
-  console.log(`quizId: ${quizId}. userId: ${userId}`);
   return await model.aggregate([
-    // Match the documents to only use the given quizId and userId
     {
       $match: {
-        // Convert id to ObjectId (needed for $match)
         quiz: mongoose.Types.ObjectId.createFromHexString(quizId),
         user: mongoose.Types.ObjectId.createFromHexString(userId),
       },
     },
-    // Group and get the max attempt number
     {
       $group: {
         _id: { quiz_question: "$quiz_question", user: "$user" },
         greatestAttempt: { $max: "$attempt" },
       },
     },
-    // Lookup documents that match the greatest attempt for each group
     {
       $lookup: {
         from: "answers",
@@ -52,11 +47,9 @@ export async function findLatestAnswersForQuiz(quizId, userId) {
         as: "latestAnswers",
       },
     },
-    // Flatten the array of matched answers (the result will be an array of answers)
     {
       $unwind: "$latestAnswers",
     },
-    // Project fields to return
     {
       $project: {
         _id: "$latestAnswers._id",
